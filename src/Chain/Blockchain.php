5677d96510cbf4e7d631d6cac32a1127941212a3
arthursiq5 <arthur.siq5@gmail.com>
2021-12-10 12:56:38 -0300

    Adicionado teste para verificar se a chain possui apenas valores válidos

--- src/Chain/Blockchain.php
+++ src/Chain/Blockchain.php
@@ -5,9 +5,12 @@ namespace Arthursiq5\PhpSimplechain\Chain;
 use Arthursiq5\PhpSimplechain\Struct\BaseBlock as Block;
 use Arthursiq5\PhpSimplechain\Struct\BlockInterface;
 
+/**
+ * @property Block[] $blocks
+ */
 class Blockchain implements BlockchainInterface
 {
-    private $blocks = [];
+    private array $blocks = [];
 
     public function __construct(Block $genesisBlock)
     {
@@ -17,6 +20,16 @@ class Blockchain implements BlockchainInterface
     public function add(BlockInterface $block): void
     {
         $this->blocks[] = $block;
+    }
+    public function isValid(): bool
+    {
+        $count = count($this->blocks()) -1;
+        for($i = 0; $i < $count; $i++) {
+            if (!$this->blocks[$i]->isNextValid($this->blocks[$i + 1])) {
+                return false;
+            }
+        }
+        return true;
     }
     public function last(): Block
     {
--- tests/Chain/BlockchainTest.php
+++ tests/Chain/BlockchainTest.php
@@ -25,4 +25,13 @@ class BlockchainTest extends TestCase
         $this->assertEquals($this->blockchain->last()->hash, $newBlock->hash);
         $this->assertEquals($this->blockchain->last()->index, $newBlock->index);
     }
+
+    public function test_blockchain_is_valid(): void
+    {
+        $lastBlock = $this->blockchain->last();
+        $newBlock = new Block($lastBlock->index + 1, "c666e1a82b8627690120cc43dbe79e9ec94ba5c3f6207d7c2f53cfa03e9db0b9", 'My test block');
+
+        $this->blockchain->add($newBlock);
+        $this->assertFalse($this->blockchain->isValid());
+    }
 }
